5f1b7875ed3b8a94cf0f978bda7b65e611cc6545
Petr Sheshenya <pyotr.sh@gmail.com>
2019-04-01 01:46:17 +0300

    Added Tests

---
+++ src/test/java/my/sheshenya/samplenettyrestfulapi/logic/SimpleTransactionListenerTest.java
@@ -0,0 +1,17 @@
+package my.sheshenya.samplenettyrestfulapi.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleTransactionListenerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void doTransaction() {
+    }
+}
