23628d2533345a65a30c074e9abcf67ae502c08f
Petr Sheshenya <pyotr.sh@gmail.com>
2019-04-02 23:57:47 +0300

    use lombok

--- src/main/java/my/sheshenya/samplenettyrestfulapi/model/Transaction.java
+++ src/main/java/my/sheshenya/samplenettyrestfulapi/model/Transaction.java
@@ -1,13 +1,16 @@
 package my.sheshenya.samplenettyrestfulapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.io.Serializable;
 import java.util.Date;
 import java.util.StringJoiner;
 
-
+@Data
+@NoArgsConstructor
 public class Transaction implements Serializable {
 
     private String id;
@@ -19,11 +22,6 @@ public class Transaction implements Serializable {
 
     private double amount;
     private String status;
-
-
-    public Transaction() {
-
-    }
 
     public Transaction(String fromAccountId, String toAccountId, double amount) {
         this.fromAccountId = fromAccountId;
@@ -52,56 +50,5 @@ public class Transaction implements Serializable {
                 .add("status="+status);
         return joiner.toString();
     }
-
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getFromAccountId() {
-        return fromAccountId;
-    }
-
-    public void setFromAccountId(String fromAccountId) {
-        this.fromAccountId = fromAccountId;
-    }
-
-    public String getToAccountId() {
-        return toAccountId;
-    }
-
-    public void setToAccountId(String toAccountId) {
-        this.toAccountId = toAccountId;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
 
 }
